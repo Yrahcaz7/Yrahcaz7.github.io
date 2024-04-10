40139c97d83ba8ad7f4882769fc9fc15a6a58e3f
Yrahcaz7 <zachary.shive@gmail.com>
2024-04-10 15:52:52 -0400

    add README and dark theme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yrahcaz7.github.io
+
+The [site](https://yrahcaz7.github.io/) contains links to my (Yrahcaz7's) games.
--- main.js
+++ main.js
@@ -9,7 +9,7 @@ window.onload = () => {
 		text += "<a href='https://yrahcaz7.github.io/" + GAMES[index].replace(/ /g, "-") + "/'>Play the Game</a><br>";
 		text += "<a href='https://github.com/Yrahcaz7/" + GAMES[index].replace(/ /g, "-") + "'>View Source Code</a></span>";
 	};
-	document.getElementById("games").innerHTML = text + "<span><br>More coming soon!<br></span>";
+	document.getElementById("games").innerHTML = text + "<span style='display: flex; align-items: center'>More coming soon!</span>";
 	text = "";
 	for (let index = 0; index < TREES.length; index++) {
 		text += "<span><b>" + TREES[index][1] + "</b><br>";
@@ -17,4 +17,23 @@ window.onload = () => {
 		text += "<a href='https://github.com/" + TREES[index][0] + "/" + TREES[index][1].replace(/ /g, "-") + "'>View Source Code</a></span>";
 	};
 	document.getElementById("trees").innerHTML = text;
+};
+
+let nightTheme = false;
+
+function changeTheme() {
+	if (nightTheme) {
+		document.getElementById("theme").innerHTML = "Activate Dark Theme";
+		document.documentElement.style.setProperty("--bg-color", "#F0F0F0");
+		document.documentElement.style.setProperty("--txt-color", "#101010");
+		document.documentElement.style.setProperty("--link-color-1", "#0000EE");
+		document.documentElement.style.setProperty("--link-color-2", "#551A8B");
+	} else {
+		document.getElementById("theme").innerHTML = "Activate Light Theme";
+		document.documentElement.style.setProperty("--bg-color", "#101010");
+		document.documentElement.style.setProperty("--txt-color", "#F0F0F0");
+		document.documentElement.style.setProperty("--link-color-1", "#EDED00");
+		document.documentElement.style.setProperty("--link-color-2", "#528C1B");
+	};
+	nightTheme = !nightTheme;
 };
