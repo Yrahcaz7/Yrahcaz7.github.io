e6f88a547df600c28bde2621401fcbfccaa206a7
Yrahcaz7 <zachary.shive@gmail.com>
2024-04-16 22:59:01 -0400

    add descriptions and tools section

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-const GAMES = ["Formula Clicker", "Dungeon of Souls"];
+const GAMES = [["Formula Clicker", "A simplistic<br>clicker game"], ["Dungeon of Souls", "A turn-based roguelike<br>deck-builder game"]];
 
 const TREES = [["Yrahcaz7", "The Primordial Tree"], ["more-repo-forks", "Adaptation Tree"], ["more-repo-forks", "Booster-Generator Tree"]];
+
+const TOOLS = ["Ancient Code Converter"];
 
 window.onload = () => {
 	let text = "";
 	for (let index = 0; index < GAMES.length; index++) {
-		text += "<span><b>" + GAMES[index] + "</b><br>";
-		text += "<a href='https://yrahcaz7.github.io/" + GAMES[index].replace(/ /g, "-") + "/'>Play the Game</a><br>";
-		text += "<a href='https://github.com/Yrahcaz7/" + GAMES[index].replace(/ /g, "-") + "'>View Source Code</a></span>";
+		text += "<span><b>" + GAMES[index][0] + "</b><br>";
+		text += GAMES[index][1] + "<br>";
+		text += "<a href='https://yrahcaz7.github.io/" + GAMES[index][0].replace(/ /g, "-") + "/'>Play the Game</a><br>";
+		text += "<a href='https://github.com/Yrahcaz7/" + GAMES[index][0].replace(/ /g, "-") + "'>View Source Code</a></span>";
 	};
 	document.getElementById("games").innerHTML = text + "<span style='display: flex; align-items: center'>More coming soon!</span>";
 	text = "";
@@ -17,6 +20,13 @@ window.onload = () => {
 		text += "<a href='https://github.com/" + TREES[index][0] + "/" + TREES[index][1].replace(/ /g, "-") + "'>View Source Code</a></span>";
 	};
 	document.getElementById("trees").innerHTML = text;
+	text = "";
+	for (let index = 0; index < TOOLS.length; index++) {
+		text += "<span><b>" + TOOLS[index] + "</b><br>";
+		text += "<a href='https://yrahcaz7.github.io/" + TOOLS[index].replace(/ /g, "-") + "/'>Use the Tool</a><br>";
+		text += "<a href='https://github.com/Yrahcaz7/" + TOOLS[index].replace(/ /g, "-") + "'>View Source Code</a></span>";
+	};
+	document.getElementById("tools").innerHTML = text;
 };
 
 let nightTheme = false;
@@ -32,8 +42,8 @@ function changeTheme() {
 		document.getElementById("theme").innerHTML = "Activate Light Theme";
 		document.documentElement.style.setProperty("--bg-color", "#101010");
 		document.documentElement.style.setProperty("--txt-color", "#F0F0F0");
-		document.documentElement.style.setProperty("--link-color-1", "#EDED00");
-		document.documentElement.style.setProperty("--link-color-2", "#528C1B");
+		document.documentElement.style.setProperty("--link-color-1", "#EEEE00");
+		document.documentElement.style.setProperty("--link-color-2", "#508B1A");
 	};
 	nightTheme = !nightTheme;
 };
