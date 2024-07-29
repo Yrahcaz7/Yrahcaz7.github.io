bd91d021a1eb4867d614243fe045acb5e4eecbba
Yrahcaz7 <zachary.shive@gmail.com>
2024-07-29 15:50:29 -0400

    add more descriptions & implement stars

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-const GAMES = [["Formula Clicker", "A simplistic<br>clicker game"], ["Dungeon of Souls", "A turn-based roguelike<br>deck-builder game"]];
+const GAMES = [["Formula Clicker", "A simplistic<br>clicker game", true], ["Dungeon of Souls", "A turn-based roguelike<br>deck-builder game"]];
 
 const TREES = [["Yrahcaz7", "The Primordial Tree"], ["more-repo-forks", "Adaptation Tree"], ["more-repo-forks", "Booster-Generator Tree"]];
 
-const TOOLS = ["Ancient Code Converter"];
+const TOOLS = [["Ancient Code Converter", "A code converter for<br>mathmatical expressions"]];
 
 window.onload = () => {
 	let text = "";
 	for (let index = 0; index < GAMES.length; index++) {
-		text += "<span><b>" + GAMES[index][0] + "</b><br>";
-		text += GAMES[index][1] + "<br>";
-		text += "<a href='https://yrahcaz7.github.io/" + GAMES[index][0].replace(/ /g, "-") + "/'>Play the Game</a><br>";
-		text += "<a href='https://github.com/Yrahcaz7/" + GAMES[index][0].replace(/ /g, "-") + "'>View Source Code</a></span>";
+		text += "<span class='item'>";
+		if (GAMES[index][2]) text += "<div class='star'></div>";
+		text += "<div><b>" + GAMES[index][0] + "</b><br>";
+		if (GAMES[index][1]) text += GAMES[index][1] + "<br>";
+		text += "<a href='https://yrahcaz7.github.io/" + GAMES[index][0].replace(/\s/g, "-") + "/'>Play the Game</a><br>";
+		text += "<a href='https://github.com/Yrahcaz7/" + GAMES[index][0].replace(/\s/g, "-") + "'>View Source Code</a></div></span>";
 	};
-	document.getElementById("games").innerHTML = text + "<span style='display: flex; align-items: center'>More coming soon!</span>";
+	document.getElementById("games").innerHTML = text + "<span class='item'><div>More coming soon!</div></span>";
 	text = "";
 	for (let index = 0; index < TREES.length; index++) {
-		text += "<span><b>" + TREES[index][1] + "</b><br>";
-		text += "<a href='https://" + TREES[index][0][0].toLowerCase() + TREES[index][0].slice(1) + ".github.io/" + TREES[index][1].replace(/ /g, "-") + "/'>Play the Game</a><br>";
-		text += "<a href='https://github.com/" + TREES[index][0] + "/" + TREES[index][1].replace(/ /g, "-") + "'>View Source Code</a></span>";
+		text += "<span class='item'><div><b>" + TREES[index][1] + "</b><br>";
+		if (TREES[index][2]) text += TREES[index][2] + "<br>";
+		text += "<a href='https://" + TREES[index][0][0].toLowerCase() + TREES[index][0].slice(1) + ".github.io/" + TREES[index][1].replace(/\s/g, "-") + "/'>Play the Game</a><br>";
+		text += "<a href='https://github.com/" + TREES[index][0] + "/" + TREES[index][1].replace(/\s/g, "-") + "'>View Source Code</a></div></span>";
 	};
 	document.getElementById("trees").innerHTML = text;
 	text = "";
 	for (let index = 0; index < TOOLS.length; index++) {
-		text += "<span><b>" + TOOLS[index] + "</b><br>";
-		text += "<a href='https://yrahcaz7.github.io/" + TOOLS[index].replace(/ /g, "-") + "/'>Use the Tool</a><br>";
-		text += "<a href='https://github.com/Yrahcaz7/" + TOOLS[index].replace(/ /g, "-") + "'>View Source Code</a></span>";
+		text += "<span class='item'><div><b>" + TOOLS[index][0] + "</b><br>";
+		if (TOOLS[index][1]) text += TOOLS[index][1] + "<br>";
+		text += "<a href='https://yrahcaz7.github.io/" + TOOLS[index][0].replace(/\s/g, "-") + "/'>Use the Tool</a><br>";
+		text += "<a href='https://github.com/Yrahcaz7/" + TOOLS[index][0].replace(/\s/g, "-") + "'>View Source Code</a></div></span>";
 	};
 	document.getElementById("tools").innerHTML = text;
 };
