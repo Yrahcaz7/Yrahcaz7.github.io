0838ad8aef0361280f37d7eaa2a920067d328c18
Yrahcaz7 <zachary.shive@gmail.com>
2024-10-02 20:29:27 -0400

    add save manager

--- main.js
+++ main.js
@@ -37,17 +37,104 @@ let nightTheme = false;
 
 function changeTheme() {
 	if (nightTheme) {
-		document.getElementById("theme").innerHTML = "Activate Dark Theme";
+		document.getElementById("theme").innerHTML = "Switch to Dark Theme";
 		document.documentElement.style.setProperty("--bg-color", "#F0F0F0");
 		document.documentElement.style.setProperty("--txt-color", "#101010");
 		document.documentElement.style.setProperty("--link-color-1", "#0000EE");
 		document.documentElement.style.setProperty("--link-color-2", "#551A8B");
+		document.documentElement.style.setProperty("--table-color", "#10F0F040");
 	} else {
-		document.getElementById("theme").innerHTML = "Activate Light Theme";
+		document.getElementById("theme").innerHTML = "Switch to Light Theme";
 		document.documentElement.style.setProperty("--bg-color", "#101010");
 		document.documentElement.style.setProperty("--txt-color", "#F0F0F0");
 		document.documentElement.style.setProperty("--link-color-1", "#EEEE00");
 		document.documentElement.style.setProperty("--link-color-2", "#508B1A");
+		document.documentElement.style.setProperty("--table-color", "#F0101040");
 	};
 	nightTheme = !nightTheme;
+};
+
+function title(str) {
+	let result = "";
+	for (let num = 0; num < str.length; num++) {
+		if (num == 0 || (/\s/.test(str.charAt(num - 1)) && !/^(a|an|and|at|but|by|for|in|nor|of|on|or|so|the|to|up|yet)(?!\w)/.test(str.substring(num)))) result += str.charAt(num).toUpperCase();
+		else result += str.charAt(num);
+	};
+	return result;
+};
+
+let tools = ["Ancient Code Converter", "Ultra Timetable", "Thing Manager"];
+
+function openSaveManager() {
+	if (!document.getElementById("save_manager")) {
+		let popup = document.createElement("dialog");
+		popup.id = "save_manager";
+		let html = "<div>These are your save data items for all Yrahcaz7 games/tools.<br>You may choose to delete any of them, wiping the saved data.</div><table><tr><th>Game/Tool</th><th>Data Type</th><th>Action</th></tr>";
+		for (let index = 0; index < localStorage.length; index++) {
+			let key = localStorage.key(index);
+			if (/Yrahcaz7/i.exec(key)) {
+				let type = "Game progress";
+				key = key.replace(/Yrahcaz7|ModTree/gi, "");
+				if (key.includes("options")) {
+					key = key.replace(/options/i, "");
+					type = "Options/Settings";
+				} else if (key.includes("save/0")) {
+					key = key.replace(/save\/0/i, "");
+					type = "Current run progress";
+				} else if (key.includes("save/master")) {
+					key = key.replace(/save\/master/i, "");
+					type = "Overall progress";
+				} else {
+					key = key.replace(/save/i, "");
+				};
+				key = title(key.replace(/([a-z])([A-Z])/g, "$1 $2").replace(/[^A-Za-z0-9]/g, " ").trim());
+				if (key == "Booster Generator Tree") key = "Booster-Generator Tree";
+				if (tools.includes(key)) type = "Tool save data";
+				html += "<tr><td>" + key + "</td><td>" + type + "</td><td><select id='save_" + index + "_action'><option value='0'>Keep</option><option value='1'>Delete</option></select></td></tr>";
+			};
+		};
+		html += "</table><div class='flex'><button class='item' onclick='closeSaveManager()'>Cancel</button><button class='item' onclick='confirmApplyManagement()'>Apply</button></div>";
+		popup.innerHTML = html;
+		document.body.append(popup);
+		popup.showModal();
+	} else {
+		for (let index = 0; index < localStorage.length; index++) {
+			let key = localStorage.key(index);
+			if (/Yrahcaz7/i.exec(key)) document.getElementById("save_" + index + "_action").value = "0";
+		};
+	};
+	document.getElementById("save_manager").showModal();
+};
+
+function closeSaveManager() {
+	if (document.getElementById("save_manager")) document.getElementById("save_manager").close();
+};
+
+function confirmApplyManagement() {
+	if (!document.getElementById("confirm_save_management")) {
+		let popup = document.createElement("dialog");
+		popup.id = "confirm_save_management";
+		popup.innerHTML = "<div>Are you really sure you want to apply these changes?</div><div class='flex' style='margin-top: 1rem'><button class='item' onclick='closeConfirmation()'>No, go back</button><button class='item' onclick='applyChanges()'>Yes, I am sure</button></div>";
+		document.body.append(popup);
+		popup.showModal();
+	} else {
+		for (let index = 0; index < localStorage.length; index++) {
+			let key = localStorage.key(index);
+			if (/Yrahcaz7/i.exec(key)) document.getElementById("save_" + index + "_action").value = "0";
+		};
+	};
+	document.getElementById("confirm_save_management").showModal();
+};
+
+function closeConfirmation() {
+	if (document.getElementById("confirm_save_management")) document.getElementById("confirm_save_management").close();
+};
+
+function applyChanges() {
+	for (let index = localStorage.length - 1; index >= 0; index--) {
+		let key = localStorage.key(index);
+		if (/Yrahcaz7/i.exec(key) && document.getElementById("save_" + index + "_action").value > 0) localStorage.removeItem(key);
+	};
+	document.getElementById("confirm_save_management").remove();
+	document.getElementById("save_manager").remove();
 };
